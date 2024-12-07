aed9d6b2b691358c920e9865bf2f931b33ec3123
Dhanush CK <dhanushck01@gmail.com>
2024-12-07 23:55:59 +0530

    feat : Made the element follow the cursor

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,12 @@ eventList.addEventListener('wheel', (event)=>{
         eventList.scrollLeft += event.deltaY;        
     }
 })
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e)=> {
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+})
