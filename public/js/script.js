00173c5dc82d758b576ff3eb54514a6615c38a65
Dhanush CK <dhanushck01@gmail.com>
2024-12-09 01:18:27 +0530

    feat : Made custom cursor work

--- public/js/script.js
+++ public/js/script.js
@@ -8,10 +8,54 @@ eventList.addEventListener('wheel', (event)=>{
 })
 
 const cursor = document.querySelector('.cursor');
+const header2 = document.querySelectorAll('a');
 
-document.addEventListener('mousemove', (e)=> {
+function growCursor() {
+    // cursor.style.mixBlendMode = 'difference';
+    cursor.style.backgroundColor = 'transparent'
+    cursor.style.border = '.5px  white dashed'
+    cursor.style.scale = '5';
+    isActive = true;
+}
+
+function shrinkCursor() {
+    // cursor.style.mixBlendMode = 'normal';
+    cursor.style.backgroundColor = 'white'
+    cursor.style.scale = '1';
+    isActive = false;
+}
+
+header2.forEach((header) => {
+    header.addEventListener('mousemove', growCursor)
+    header.addEventListener('mouseleave', shrinkCursor)
+})
+
+window.addEventListener('mousemove', (e)=> {
     var x = e.clientX;
     var y = e.clientY;
     cursor.style.left = x + 'px';
     cursor.style.top = y + 'px';
 })
+
+var timeout;
+var isHidden = false;
+var isActive = false;
+
+document.addEventListener("mousemove", magicMouse);
+
+function magicMouse() {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(function() {
+        if (!isHidden && !isActive) {
+            cursor.style.display = 'none';
+            isHidden = true;
+        }
+    }, 1000);
+    if (isHidden) {
+        cursor.style.display = 'block';
+        isHidden = false;
+    }
+};
+
